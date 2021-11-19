10d4efa64737f0c85cd33341dec6905060eb8a80
move <move@zealand.dk>
2021-11-19 10:29:59 +0100

    Added comment

--- CarLibrary/Car.cs
+++ CarLibrary/Car.cs
@@ -2,6 +2,8 @@
 
 namespace CarLibrary
 {
+    //Remember always to have an empty constructor in a Class that could be serialized
+    //If no constructor is defined, an empty constructor is default
     public class Car
     {
         public string Model { get; set; }
