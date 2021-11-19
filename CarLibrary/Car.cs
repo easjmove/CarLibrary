f4e49c69b2a874e0589d949421e3b23b29b725f0
move <move@zealand.dk>
2021-11-19 08:56:33 +0100

    Add project files.

---
+++ CarLibrary/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CarLibrary
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string RegistrationNumber { get; set; }
+    }
+}
